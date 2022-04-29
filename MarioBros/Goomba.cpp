745be560f79d957d27e5c455ccbd89b0fcd61f04
tuanhuyngt <tuanhuyngt@gmail.com>
2022-04-29 17:05:27 +0700

    Add goomba sprite + render

--- MarioBros/Goomba.cpp
+++ MarioBros/Goomba.cpp
@@ -1,26 +1,32 @@
 #include "Goomba.h"
+#include "debug.h"
 
-CGoomba::CGoomba(float x, float y):CGameObject(x, y)
+CGoomba::CGoomba(float x, float y, int Level) :CGameObject(x, y)
 {
 	this->ax = 0;
 	this->ay = GOOMBA_GRAVITY;
 	die_start = -1;
 	SetState(GOOMBA_STATE_WALKING);
+	level = Level;
+	if (level == PARA_GOOMBA)ParaGoomba = true;
+	else ParaGoomba = false;
+	goombaPhase = 1;
+	phaseTime = 0;
 }
 
-void CGoomba::GetBoundingBox(float &left, float &top, float &right, float &bottom)
+void CGoomba::GetBoundingBox(float& left, float& top, float& right, float& bottom)
 {
 	if (state == GOOMBA_STATE_DIE)
 	{
-		left = x - GOOMBA_BBOX_WIDTH/2;
-		top = y - GOOMBA_BBOX_HEIGHT_DIE/2;
+		left = x - GOOMBA_BBOX_WIDTH / 2;
+		top = y - GOOMBA_BBOX_HEIGHT_DIE / 2;
 		right = left + GOOMBA_BBOX_WIDTH;
 		bottom = top + GOOMBA_BBOX_HEIGHT_DIE;
 	}
 	else
-	{ 
-		left = x - GOOMBA_BBOX_WIDTH/2;
-		top = y - GOOMBA_BBOX_HEIGHT/2;
+	{
+		left = x - GOOMBA_BBOX_WIDTH / 2;
+		top = y - GOOMBA_BBOX_HEIGHT / 2;
 		right = left + GOOMBA_BBOX_WIDTH;
 		bottom = top + GOOMBA_BBOX_HEIGHT;
 	}
@@ -32,12 +38,12 @@ void CGoomba::OnNoCollision(DWORD dt)
 	y += vy * dt;
 };
 
-void CGoomba::OnCollisionWith(LPCOLLISIONEVENT e)
+void CGoomba::OnCollisionWith(LPCOLLISIONEVENT e, DWORD dt)
 {
-	if (!e->obj->IsBlocking()) return; 
-	if (dynamic_cast<CGoomba*>(e->obj)) return; 
+	if (!e->obj->IsBlocking()) return;
+	if (dynamic_cast<CGoomba*>(e->obj)) return;
 
-	if (e->ny != 0 )
+	if (e->ny != 0)
 	{
 		vy = 0;
 	}
@@ -45,20 +51,23 @@ void CGoomba::OnCollisionWith(LPCOLLISIONEVENT e)
 	{
 		vx = -vx;
 	}
+	if (goombaPhase == GOOMBA_PHASE_JUMPING && level == PARA_GOOMBA)
+	{
+		if (e->ny < 0) vy = -GOOMBA_JUMPING_SPEED;
+	}
 }
 
-void CGoomba::Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects)
+void CGoomba::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 {
 	vy += ay * dt;
 	vx += ax * dt;
 
-	if ( (state==GOOMBA_STATE_DIE) && (GetTickCount64() - die_start > GOOMBA_DIE_TIMEOUT) )
+	if ((state == GOOMBA_STATE_DIE) && (GetTickCount64() - die_start > GOOMBA_DIE_TIMEOUT))
 	{
 		isDeleted = true;
 		return;
 	}
-
-	CGameObject::Update(dt, coObjects);
+	if (level == PARA_GOOMBA)CalcGoombaMove();
 	CCollision::GetInstance()->Process(this, dt, coObjects);
 }
 
@@ -66,13 +75,15 @@ void CGoomba::Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects)
 void CGoomba::Render()
 {
 	int aniId = ID_ANI_GOOMBA_WALKING;
-	if (state == GOOMBA_STATE_DIE) 
-	{
-		aniId = ID_ANI_GOOMBA_DIE;
+	if (ParaGoomba)GetParaGoombaAni(aniId);
+	else {
+		if (state == GOOMBA_STATE_DIE)
+		{
+			aniId = ID_ANI_GOOMBA_DIE;
+		}
 	}
-
-	CAnimations::GetInstance()->Get(aniId)->Render(x,y);
-	RenderBoundingBox();
+	CAnimations::GetInstance()->Get(aniId)->Render(x, y);
+	//RenderBoundingBox();
 }
 
 void CGoomba::SetState(int state)
@@ -80,15 +91,15 @@ void CGoomba::SetState(int state)
 	CGameObject::SetState(state);
 	switch (state)
 	{
-		case GOOMBA_STATE_DIE:
-			die_start = GetTickCount64();
-			y += (GOOMBA_BBOX_HEIGHT - GOOMBA_BBOX_HEIGHT_DIE)/2;
-			vx = 0;
-			vy = 0;
-			ay = 0; 
-			break;
-		case GOOMBA_STATE_WALKING: 
-			vx = -GOOMBA_WALKING_SPEED;
-			break;
+	case GOOMBA_STATE_DIE:
+		die_start = GetTickCount64();
+		y += (GOOMBA_BBOX_HEIGHT - GOOMBA_BBOX_HEIGHT_DIE) / 2;
+		vx = 0;
+		vy = 0;
+		ay = 0;
+		break;
+	case GOOMBA_STATE_WALKING:
+		vx = GOOMBA_WALKING_SPEED;
+		break;
 	}
 }
--- MarioBros/Goomba.h
+++ MarioBros/Goomba.h
@@ -1,9 +1,17 @@
 #pragma once
 #include "GameObject.h"
 
-#define GOOMBA_GRAVITY 0.002f
+#define GOOMBA_GRAVITY 0.0007f
 #define GOOMBA_WALKING_SPEED 0.05f
+#define GOOMBA_JUMPING_SPEED 0.1f
+#define GOOMBA_FLYING_SPEED 0.25f
 
+#define GOOMBA_PHASE_WALKING	1
+#define GOOMBA_PHASE_JUMPING	2
+#define GOOMBA_PHASE_FLYING		3
+
+#define NORMAL_GOOMBA	1
+#define PARA_GOOMBA		2
 
 #define GOOMBA_BBOX_WIDTH 16
 #define GOOMBA_BBOX_HEIGHT 14
@@ -14,28 +22,88 @@
 #define GOOMBA_STATE_WALKING 100
 #define GOOMBA_STATE_DIE 200
 
-#define ID_ANI_GOOMBA_WALKING 5000
-#define ID_ANI_GOOMBA_DIE 5001
+#define ID_ANI_GOOMBA_WALKING 20000
+#define ID_ANI_GOOMBA_DIE 21000
+
+#define ID_ANI_PARAGOOMBA_WALKING 20001
+#define ID_ANI_PARAGOOMBA_JUMPING 20002
+#define ID_ANI_PARAGOOMBA_FLYING 20003
+#define ID_ANI_PARAGOOMBA_NOWING_WALKING 20004
+#define ID_ANI_PARAGOOMBA_DEAD	21001
+
+
 
 class CGoomba : public CGameObject
 {
 protected:
-	float ax;				
-	float ay; 
+	float ax;
+	float ay;
+
+	bool ParaGoomba;
+
+	DWORD phaseTime;
 
 	ULONGLONG die_start;
 
-	virtual void GetBoundingBox(float &left, float &top, float &right, float &bottom);
-	virtual void Update(DWORD dt, vector<LPGAMEOBJECT> *coObjects);
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom);
+	virtual void Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects);
 	virtual void Render();
 
 	virtual int IsCollidable() { return 1; };
 	virtual int IsBlocking() { return 0; }
 	virtual void OnNoCollision(DWORD dt);
 
-	virtual void OnCollisionWith(LPCOLLISIONEVENT e);
+	virtual void OnCollisionWith(LPCOLLISIONEVENT e, DWORD dt);
 
-public: 	
-	CGoomba(float x, float y);
+	void CalcGoombaMove() {
+		switch (goombaPhase) {
+		case GOOMBA_PHASE_WALKING:
+		{
+			if (phaseTime == 0) {
+				phaseTime = GetTickCount64();
+			}
+			else if (GetTickCount64() - phaseTime > 2000) {
+				phaseTime = 0;
+				goombaPhase = GOOMBA_PHASE_JUMPING;
+			}
+			break;
+		}
+		case GOOMBA_PHASE_JUMPING: {
+			if (phaseTime == 0) {
+				phaseTime = GetTickCount64();
+			}
+			else if (GetTickCount64() - phaseTime > 1500) {
+				phaseTime = 0;
+				goombaPhase = GOOMBA_PHASE_FLYING;
+			}
+			break;
+		}
+		case GOOMBA_PHASE_FLYING: {
+			if (phaseTime == 0) {
+				phaseTime = GetTickCount64();
+				vy = -GOOMBA_FLYING_SPEED;
+			}
+			else if (GetTickCount64() - phaseTime > 1000) {
+				phaseTime = 0;
+				goombaPhase = GOOMBA_PHASE_WALKING;
+			}
+			break;
+		}
+		}
+	}
+
+	void GetParaGoombaAni(int& idAni) {
+		if (level == PARA_GOOMBA)
+		{
+			if (goombaPhase == GOOMBA_PHASE_WALKING)idAni = ID_ANI_PARAGOOMBA_WALKING;
+			else if (goombaPhase == GOOMBA_PHASE_JUMPING)idAni = ID_ANI_PARAGOOMBA_JUMPING;
+			else if (goombaPhase == GOOMBA_PHASE_FLYING)idAni = ID_ANI_PARAGOOMBA_FLYING;
+		}
+		else if (state == GOOMBA_STATE_WALKING)idAni = ID_ANI_PARAGOOMBA_NOWING_WALKING;
+		else idAni = ID_ANI_PARAGOOMBA_DEAD;
+	};
+public:
+	int level, goombaPhase;
+	CGoomba(float x, float y, int Level);
 	virtual void SetState(int state);
 };
