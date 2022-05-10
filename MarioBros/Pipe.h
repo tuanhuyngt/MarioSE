9d1326e3ee74d5079a302d71ad032d363e8f3205
tuanhuyngt <tuanhuyngt@gmail.com>
2022-05-10 23:57:07 +0700

    Adding Firebullet sprite + pipe render.

--- MarioBros/Pipe.h
+++ MarioBros/Pipe.h
@@ -1,5 +1,10 @@
 #pragma once
 #include "GameObject.h"
+
+#define ID_SPRITE_PIPE_MOUTH 131313
+#define ID_SPRITE_PIPE_BODY 141414
+#define SPRITE_TILE_SIZE	16
+
 class Pipe :
 	public CGameObject
 {
@@ -9,7 +14,18 @@ public:
 		Width = width;
 		Height = height;
 	}
-	void Render() {};
+	void Render() {
+		CSprites* sprite = CSprites::GetInstance();
+		int SpriteY = y - Height / 2 + SPRITE_TILE_SIZE / 2;
+		for (int i = 0; i < Height / SPRITE_TILE_SIZE; i++)
+		{
+			if (i < 1)
+				sprite->Get(ID_SPRITE_PIPE_MOUTH)->Draw(x, SpriteY);
+			else
+				sprite->Get(ID_SPRITE_PIPE_BODY)->Draw(x, SpriteY);
+			SpriteY += SPRITE_TILE_SIZE;
+		}
+	};
 	void Update(DWORD dt) {
 	}
 	void GetBoundingBox(float& l, float& t, float& r, float& b);
